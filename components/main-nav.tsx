9ff07c3eeb1f71c9d50aba09f8fbf098ec25e099
林庭楷 <hellolinpeter@gmail.com>
2024-06-17 13:48:29 +0800

    feat:site header

---
+++ components/main-nav.tsx
@@ -0,0 +1,29 @@
+"use client"
+
+import Link from "next/link";
+import {siteConfig} from "@/config/site";
+import {cn} from "@/lib/utils";
+import {usePathname} from "next/navigation";
+import {Icons} from "@/components/icon";
+
+export function MainNav() {
+    const pathname = usePathname()
+    return (
+        <nav className="flex items-center space-x-4 lg:space-x-6">
+            {/* Logo */}
+            <Link href="/" className="mr-6 flex items-center space-x-2">
+                {/*<Icons.logo className="h-8 w-8"/>*/}
+                <span className="font-bold">{siteConfig.name}</span>
+            </Link>
+            <Link href="/blog" className={cn("text-sm font-medium transition-colors hover:text-primary",
+                pathname === "/blog" ? "text-foreground" : "text-foreground/60")}>
+                Blog
+            </Link>
+
+            <Link href="/about" className={cn("text-sm font-medium transition-colors hover:text-primary",
+                pathname === "/about" ? "text-foreground" : "text-foreground/60")}>
+                About
+            </Link>
+        </nav>
+    )
+}
---
+++ config/site.ts
@@ -0,0 +1,12 @@
+export const siteConfig = {
+    name: "Peter",
+    url: "https://example.com",
+    description: "Nextjs 14",
+    authors: "Peter",
+    links: {
+        instagram:"",
+        github:"",
+    }
+}
+
+export type SiteConfig = typeof siteConfig
